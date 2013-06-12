e8a47fb28af6a32193b517594e84673653319059
Chandrasekhar Thotakura <chandrasekhar6t@gmail.com>
2013-06-12 13:39:23 -0400

    Remove console logs

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
 var express = require('express'),
 	app = express(),
 	oneDay = 86400000,
-	port = process.env.PORT || 8000;
+	port = process.env.PORT || 8000,
+    ip = process.env.IP;
 
 // compress the content before serving
 app.use(express.compress());
 
-// app.use('/parent-folder', express.static(__dirname + '/public', {maxAge: oneDay}));
+//app.use('/parent-folder', express.static(__dirname + '/public', {maxAge: oneDay}));
 app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
 
 // listen to the port
-app.listen(port, process.env.IP);
+app.listen(port, ip);
 
-console.log('Your app is running at http://localhost:' + port);
+//console.log('Your app is running at http://' + ip + ':' + port);
